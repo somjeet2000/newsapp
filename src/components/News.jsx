d5f1fde886ae4f6df5c78a832068ec214351e7f4
Somjeet Srimani <somjeetsrimani2000@gmail.com>
2024-06-27 02:48:23 +0530

    Design the News and NewsItem component and fetched API from URL

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 
 export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+    };
+  }
+
+  async componentDidMount() {
+    let apiURL =
+      'https://newsapi.org/v2/top-headlines?country=in&apiKey=819835dff3bb4283be3f680e015c1c29';
+    let fetchData = await fetch(apiURL);
+    let parsedData = await fetchData.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+  }
+
   render() {
     return (
       <div className="container my-4">
-        <NewsItem />
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title.slice(0, 45)}
+                  description={
+                    element.description ? element.description.slice(0, 90) : ''
+                  }
+                  urlToImage={element.urlToImage}
+                  url={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 
 export default class NewsItem extends Component {
   render() {
+    let { title, description, urlToImage, url } = this.props;
     return (
-      <div>
-        <div className="card" style={{ width: '18rem' }}>
-          <img src="..." className="card-img-top" alt="..." />
+      <div className="my-3">
+        <div className="card" style={{ width: '18rem', height: '23rem' }}>
+          <img
+            src={
+              urlToImage
+                ? urlToImage
+                : 'https://plus.unsplash.com/premium_photo-1691223714882-57a432c4edaf?q=80&w=1781&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            className="card-img-top"
+            alt="..."
+            style={{ height: '10rem' }}
+          />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={url} target="_blank" className="btn btn-sm btn-primary">
+              Read more
             </a>
           </div>
         </div>
